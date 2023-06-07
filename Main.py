5fc74182d25714a124754b2fe401cfb92e365aeb
Juan Pablo Arano <96890735+JuanArano17@users.noreply.github.com>
2023-06-07 19:26:00 -0300

    Db changes

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
-import threading
+import multiprocessing
 from DB import MySQLConnection
 from Game import Game
-import concurrent.futures
 import random
 import time
 import json
@@ -14,51 +13,30 @@ def play_game(n):
     print(f"Partida {n} finalizada")
     return results
 
-# Funcion que permite configurar la DB con los datos de dbConfig.json
-def load_config(config_file):
-    with open(config_file, 'r') as f:
-        config = json.load(f)
-    return config
-
 # Utilizacion de la base de datos para insertar miles de resultados a la vez
 def insert_results(db_config, results):
-    with MySQLConnection(**db_config) as cnx:
+    with MySQLConnection() as cnx:
         with cnx.cursor() as cursor:
-            insert_query = "INSERT INTO nombre_tabla (estrategia, resultado) VALUES (%s, %s)"
+            insert_query = "INSERT INTO games (victoria, duracion, idEstrategia) VALUES ({results.win}, {results.duration}, {results.idEstrategia})"
             cursor.executemany(insert_query, results)
             cnx.commit()
 
 def main():
-    task_quantity = 1000
+    task_quantity = 2
     results = []
 
-    # Lock de protección ante concurrencia
-    lockResultados = threading.Lock()
-
-    # Utiliza un ThreadPoolExecutor para manejar los hilos de manera eficiente
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(play_game, i) for i in range(task_quantity)]
-        for future in concurrent.futures.as_completed(futures):
+    with multiprocessing.Pool(processes=task_quantity) as pool:
+        games = [pool.apply_async(play_game, (i,)) for i in range(task_quantity)]
+        for game in games:
             try:
-                resultado = future.result()
-                with threading.Lock():
-                    results.append(resultado)
-                print(f"La tarea {future} fue completada")
+                result = game.get()
+                results.append(result)
+                print(f"La tarea {game} fue completada")
             except Exception as e:
                 print(f"Error en la partida: {e}")
-
     print(f"Se completaron {len(results)} tareas.")
-    
-    # Inicializaacion DB
-    config = load_config('dbConfig.json')
-    db_config = {
-        'host': config.get('DB_HOST', 'localhost'),
-        'user': config.get('DB_USER', 'root'),
-        'password': config.get('DB_PASSWORD', ''),
-        'database': config.get('DB_DATABASE', 'mi_base_de_datos')
-    }
     # Ejecucion de funcion que inserta los 1000 resultados en la DB
-    insert_results(db_config, results)
+    insert_results(results)
 
 if __name__ == "__main__":
     main()
