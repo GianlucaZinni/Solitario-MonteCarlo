5971eb6735fe37ed08d5126169afa2b1cd9f0d98
Sebastian Portillo <sebaportillo012@gmail.com>
2023-06-11 14:12:12 -0300

    Added: parámetro opcional para medir time performance

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from static.Database import MySQLConnection
 from conf.Game import Game
 from conf.Deck import Deck
 import sys
+import time
 
 strategies_output = {
     1 : "El Marino",
@@ -39,7 +40,11 @@ def insert_results(results):
             cursor.executemany(insert_query, [(result.victoria, result.duracion, result.movimientos, results.mazo, results.idEstrategia) for result in results])
             cnx.commit()
 
-def main():
+def main(analyze_performance=False):
+
+    if analyze_performance:
+        start_time = time.perf_counter()
+
     task_quantity = 1  # Cantidad de procesos que se ejecutarán (siempre serán MÍNIMO 3)
     batch_size = 1  # Cantidad de procesos que se ejecutan a la vez. (Tener cuidado con la memoria RAM)
     processes = []
@@ -84,6 +89,13 @@ def main():
         for process in processes:
             process.join()
 
+
+    if analyze_performance:
+        finish_time = time.perf_counter()
+        print("\n Programa concurrente finalizado en {} segundos".format(finish_time - start_time))
+        print("---")
+
+
 if __name__ == "__main__":
-    main()
+    main(analyze_performance=True) 
     sys.exit()
